ec95c449669b014cee617ab0f4b603bdc5e32862
halikaandro <limchong0916@gmail.com>
2024-08-24 20:36:24 -0700

    chore(dependencies): Added libraries to resolve #186
    Added `langchain-unstructured` and `langchain-huggingface` to `pyproject.toml` and replaced deprecated import statements in `utility_funcs.py`

--- freestream/pages/utils/utility_funcs.py
+++ freestream/pages/utils/utility_funcs.py
@@ -11,8 +11,8 @@ import torch
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.chat_message_histories import \
     StreamlitChatMessageHistory
-from langchain_community.document_loaders import UnstructuredFileLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_unstructured import UnstructuredLoader
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.callbacks.base import BaseCallbackHandler
 from langchain_core.documents import Document
